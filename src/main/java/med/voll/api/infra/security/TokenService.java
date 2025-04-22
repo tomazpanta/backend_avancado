3d6a22e64bf00c7d6dfeccfe457928596c2246da
tomazpantadasilvaneto <tomazpanta@hotmail.com>
2025-04-22 18:01:04 -0400

    Ultima alteração

--- src/main/java/med/voll/api/infra/security/TokenService.java
+++ src/main/java/med/voll/api/infra/security/TokenService.java
@@ -3,6 +3,7 @@ package med.voll.api.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import org.springframework.beans.factory.annotation.Value;
 import med.voll.api.domain.usuario.Usuario;
 import org.springframework.stereotype.Service;
 
@@ -11,8 +12,11 @@ import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.util.Date;
 
+
 @Service
 public class TokenService {
+
+    @Value("${api.security.token.secret}")
 
     private String secret;
 
